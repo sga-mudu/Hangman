97fe6a0928814317770a0589c194f5b4d44c4010
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-01-22 21:01:43 +0800

    Initial commit

---
+++ words.js
@@ -0,0 +1,42 @@
+export const alphabetLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+export const categories = {
+    fruits: [
+        "Apple",
+        "Banana",
+        "Mango",
+        "Orange",
+        "Pineapple",
+        "Strawberry",
+        "Grapes",
+        "Watermelon",
+        "Blueberry",
+        "Peach",
+    ],
+
+    animals: [
+        "Lion",
+        "Elephant",
+        "Giraffe",
+        "Kangaroo",
+        "Dolphin",
+        "Panda",
+        "Zebra",
+        "Penguin",
+        "Tiger",
+        "Koala",
+    ],
+
+    countries: [
+        "Canada",
+        "Brazil",
+        "Japan",
+        "Germany",
+        "Australia",
+        "India",
+        "Mexico",
+        "South Africa",
+        "Italy",
+        "Egypt",
+    ],
+};
